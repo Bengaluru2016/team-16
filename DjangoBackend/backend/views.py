31c8c2632b33e0ce7d7b9c1fbf0a074250f70adb
naresh117 <nareshchilukurti@hotmail.com>
2016-07-10 00:33:00 +0530

    graph update

--- DjangoBackend/backend/views.py
+++ DjangoBackend/backend/views.py
@@ -2,21 +2,20 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-from backend.models import Investor
+from backend.models import Investor, investment_track, user
+
+
 def create_pieChart(request,investorId):
     c=Investor.objects.all().filter(Investor_Id=investorId);
     print c;
 import pygal
-def graph():
+def graph(request,investorId):
     l1 = []
-    l2 = []
-    l3 = []
-    marks = Investor.objects.values_list('investor_id__name', 'amount_invested')
+    marks = investment_track.objects.values_list('amount_invested').filter(investor_id=user.objects.get(id=investorId))
+    print marks
     for rows in marks:
-        l1.append(rows[1])
-    bar = pygal.Bar()
-    bar.add('minimum', l1)
-    bar.add('average', l2)
-    bar.add('maximum', l3)
-    bar.render_to_file('graph.svg')
-    bar.render()
+        print rows
+        l1.append(rows[0])
+    line_chart = pygal.Line()
+    line_chart.add("investment",l1)
+    line_chart.render_to_file('graph.svg')
